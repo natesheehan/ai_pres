347c0258dcbdb6168c335db6a171293a3e2f9e47
Sheehan <nathanaelsheehan@gmail.com>
2023-11-06 08:50:18 +0000

    update slides

---
+++ src/routes/+page.svelte
@@ -0,0 +1,281 @@
+<body>
+  <div class="terminal">
+    <div class="header">Terminal of Inquiry</div>
+    <div class="container">
+      <p>ns651@5CG1275NG3 MINGW32 ~/Documents/pres $ run.pres</p>
+      <div class="workshop-info">
+        <h2>Philosophy of AI and Digital Infrastructures</h2>
+        <p>Name: Nathanael Sheehan</p>
+        <p>Talk title: Data Superstructures</p>
+        <div class="images">
+          <img src="Phil_OS.png" alt="Philosophy of AI">
+          <img src="jceei-home.png" alt="Joint Workshop">
+        </div>
+      </div>
+      
+      
+<div style="border: limegreen dashed 4px;">
+  
+<!-- Infrastructuring is a term that has been widely used within Science and technology studies as a way to define infrastructures as a processual phenomenon which work recursively with the assemblages of the actors and communities within its network of coordination, collaboration and cooperation. Accordingly, Infrastructuring places emphasis on the network as an active verb or a participatory practice of design and research (Karasti 2014). 
+Infrastructuring, as Star puts it 
+“is in a way a call to study boring things.”
+Lists of numbers, technical documentation, hidden mechanisms effecting processes 
+Infrastructure in the built environment sense, is often thought of as a system of substrates – roads, train tracks, plumbing, pile ons etc. Something that is part of the background and enables other kinds of work. This becomes a lot more complex when one begins to investigate large socio-technical data infrastructures with historical legacies that are in the making. For a molecular biologist affiliated with a renowned university in the Western hemisphere, such data infrastructures serve as prolific sources for data and research findings. Conversely, for their counterparts situated in the Global South, these infrastructures may be viewed as spaces where contributions to the division of labor are frequently overlooked or underacknowledged. Furthermore, the concept of 'infrastructure' may hold different connotations and challenges depending on an individual's area of focus or expertise. From an analytical standpoint, infrastructure is not recognized as an entity unto itself but rather is discerned only through its relational properties and interactions within a given context.
+As Goldstien and Nost suggest, infrastructures are often 
+“the invisible things researchers take for granted but are deliberately built and maintained”
+Therefore, Ethnographically studying the practices which constitute infrastructures means situating oneself in sites of dissent and diversity, that exist in both, online and AFK reality, and to connect the materiality of a situation with the practices and environment that are made and made from it.  Understanding Infrastructuring as a political and epistemic action requires that we recognise the constraints and possibilities, as well as its constructive and destructive power.  -->
+
+  <h2 style="text-align: center;">Data Infrastructuring </h2>
+
+  <h1 style="text-align: center;font-size:9em">BORING GEEKS </h1>
+  <p style="text-align: center;">Or maybe not...</p>
+  </div>
+  
+    
+  <div style="border: limegreen dashed 4px;">
+  
+    <!-- Infrastructuring is a term that has been widely used within Science and technology studies as a way to define infrastructures as a processual phenomenon which work recursively with the assemblages of the actors and communities within its network of coordination, collaboration and cooperation. Accordingly, Infrastructuring places emphasis on the network as an active verb or a participatory practice of design and research (Karasti 2014). 
+    Infrastructuring, as Star puts it 
+    “is in a way a call to study boring things.”
+    Lists of numbers, technical documentation, hidden mechanisms effecting processes 
+    Infrastructure in the built environment sense, is often thought of as a system of substrates – roads, train tracks, plumbing, pile ons etc. Something that is part of the background and enables other kinds of work. This becomes a lot more complex when one begins to investigate large socio-technical data infrastructures with historical legacies that are in the making. For a molecular biologist affiliated with a renowned university in the Western hemisphere, such data infrastructures serve as prolific sources for data and research findings. Conversely, for their counterparts situated in the Global South, these infrastructures may be viewed as spaces where contributions to the division of labor are frequently overlooked or underacknowledged. Furthermore, the concept of 'infrastructure' may hold different connotations and challenges depending on an individual's area of focus or expertise. From an analytical standpoint, infrastructure is not recognized as an entity unto itself but rather is discerned only through its relational properties and interactions within a given context.
+    As Goldstien and Nost suggest, infrastructures are often 
+    “the invisible things researchers take for granted but are deliberately built and maintained”
+    Therefore, Ethnographically studying the practices which constitute infrastructures means situating oneself in sites of dissent and diversity, that exist in both, online and AFK reality, and to connect the materiality of a situation with the practices and environment that are made and made from it.  Understanding Infrastructuring as a political and epistemic action requires that we recognise the constraints and possibilities, as well as its constructive and destructive power.  -->
+    
+      <h2 style="text-align: center;">Data Infrastructuring </h2>
+    
+      <h1 style="text-align: center;font-size:9em">CYBER BIOHACKERS </h1>
+      <p style="text-align: center;">Or maybe too far...</p>
+      </div>
+      
+<div style="border: limegreen dashed 4px;">
+  
+  <h2 style="text-align: center;">Case Study: Origins</h2>
+  <p>- Built originally by European Molecular Biology Lab (EMBL) as the `EMBL data library` at the University of Heidelberg, Germany, in 1982.</p>
+  <p>- The first release had 568 entries and was availabile for free by book or magnetic tape.</p>
+  <p>- Became a central node in the International Nucleotide Sequence Database Collaboration (INSDC).</p>
+  <p>- Continues to grow in volume with the database doubling in size every 12/18 months. </p>
+  <p>- Since the 90's has provided digital access to the data via File Transfer Protocols (FTP) servers and CD ROMS.</p>
+  <div style="display: block;margin:auto;width:60%">    
+  <img src="books.png" style="height: 140px;border:limegreen dashed 4px;" alt="">
+  <img src="insdc.png" style="height: 140px;border:limegreen dashed 4px;" alt="">
+  <img src="Web-servers-200x300.jpg" style="height: 140px;border:limegreen dashed 4px;" alt="">
+  <p style="text-align: center;">Credit: EMBL <a href="">Archive</a></p>
+
+  </div>
+</div>
+<pre style="color: limegreen;
+font-size: 0.5em;
+margin-top: 5%;
+margin: auto;
+display: block;
+width: 85%;
+padding: 5%;">
+  %%%%%%%%%%%%%%%%%%%%%%%%%
+  @@@@@@@@@@@@@@@@@@@@@@@@@  @@@@@@@@@@              @@@@@@@            @@@@@@@@@@@   %%%%%%%%%%   %%%   %%%       %%%%%%%   %%%%%%%%%%%%%
+                                    %%
+ @@@@@@@@                   @@@@@@@@@@@@@           @@@@@@@@          @@@@@@@@@@@@@@     %%%   %%%         %%%   %%%  %%%%%% %   %%%  %%%%%%%%%%
+                                %%%
+ @@@@@@@                   @@@@@@@@@@@@@@@@        @@@@@@@@        @@@@@@@@   @@@@@@@      %       %   %%%   %%%%%%%    %%%%%%%%    %%%    %%%%%%%%
+            %%%%%%%%%%%%     %%%
+@@@@@@@@@@@@@@@@@@@@@@    @@@@@@@@   @@@@@@@@      @@@@@@@       @@@@@@@@     @@@@@@@        %%%   %%% %%%%%%            %%%%%%%%%%     %%%   %%%%%%%
+%%%%%%%%%   %%%%           %%%%
+@@@@@@@@@@@@@@@@@@@@@@@    @@@@@@@      @@@@@@@@   @@@@@@@      @@@@@@@         @@@@@@@         %%%%%%%%                    %%%%%%  %%%%     %%   %%%%%  %%%%%%%%%%%%%   %%%%           %%%%%
+@@@@@@@                    @@@@@@@         @@@@@@@ @@@@@@@    @@@@@@@@@@@@@@@@@@@@@@@@@@@      %%%%%                           %%%%%%   %%%%      %%%%%%%%  %
+%%%            %%%   %%%%%
+@@@@@@@                    @@@@@@@            @@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  %%%                                   %%%%%%%%%%%%%%%%%%% %%%%%%        %%%   %%%%  %%%%%%
+@@@@@@@@@@@@@@@@@@@@@@@@@  @@@@@@@               @@@@@@@@@ @@@@@@@@                  @@@@@@@                                                              %%%%%%% %%%%   %%%%%%%%%%%
+                                                                                                                                                         %%%%%%%%%%%%%%%%%
+(Copyright © EMBL 2022)
+version:1.1.6
+</pre>
+ 
+<div style="border: limegreen dashed 4px;">
+  
+  <h2 style="text-align: center;">Case Study: Digitalisation</h2>
+  <p>Recognising the need for specialised bioinformatics infrastructure, the EMBL council inaugurated the European Bioinformatics Institute, Hixton UK, to inherit the EMBL data library's vast assets in 1994.</p>
+ <div style="text-align: center;">
+  <i>Excerpt from Interview with M_1_1a_D</i>
+  <p>I: So were you part of that transition?</p>
+  <p>
+    R: Me and M_2_1a_D, who is also working here, are the last two survivors of this time. 
+    </p>
+    <b>
+      [Respondent laughs]
+    </b>
+    <p>
+      I: Did all the magnetic tapes survive?
+    </p>
+    <p>
+R: Well, we had plans to migrate data from the MSI that our operating system runs on from Oracle to Unix. It was a complete disaster and didn't work. And so, RT_7_1a_D just put the computer into the back of his car and drove it over from Heidelberg to here and installed it. I mean, this computer at that time held less data than this phone. But it was, at this time, big data.
+  </p>
+ </div>
+
+ <div style="border: limegreen dashed 4px;">
+  <h2 style="text-align: center;">Case Study: Agglomeration</h2>
+  
+  <img src="plot.png" style="height: auto;display:block;margin:auto;width:50%;border:limegreen dashed 4px;" alt="">
+  <p style="text-align: center;">"European Nucleotide Archive: Data Diversity and Provenance", Sheehan and O'Cathil,  <a href="https://colab.research.google.com/drive/1TFS4Se61Q08rE0IGTlkXxhuTXf2pG7at#scrollTo=DBr2Tc6yB5XT"><i>forthcoming</i></a></p>
+  <p>- The data library was superseeded by the EMBL Nucleotide Sequence Database in 1995.</p>
+  <p>- EMBL Nucleotide Sequence Database would incorporate millions of entries from their neighbours at the Sanger Centre.</p>
+  <p>- New databases built on top of EMBL Nucleotide Sequence Database were being developed such as WormBase and ENSEMBL.</p>
+  <p> - EMBL Nucleotide Sequence Database gets superseeded by the European Nucleotide Archive in 2010.</p>
+  <p> - The ENA continues growing in volume, value, variety, velocity, and veracity of data at an expotential rate. </p>
+ </div>
+
+ <div style="border: limegreen dashed 4px; display: flex; align-items: center;">
+  
+  <div style="flex: 1;">
+    <h2>The GISAID Issue</h2>
+      <p>On the 29th of January 2021 the governing board of EMBL-EBI posted a public letter calling for a greater “openness” in sharing SARS-CoV-2 genome data.</p>
+      <p>The letter did not mention GISAID by name, yet it clearly targeted GISAID’s promise to swiftly release protected genomes from their data repository into one that was completely open.</p>
+  </div>
+
+  <div style="flex: 1;">
+      <img src="comparison.jpg" style="height:100%; width:100%;" alt="">
+      <p>Sheehan and Leonelli: Reconciling Data Actionability and Accountability in Global Health Research (forthcoming) </p>
+  </div>
+</div>
+<div style="border: limegreen dashed 4px; display: flex; align-items: center;">
+  
+  <div style="flex: 1;">
+    <h2>The GISAID Comparison</h2>
+      <p>GISAID depends on the comprehensiveness of its respondents and participants to be able to provide as wide-ranging a picture of global mutation patters as possible, INSDC is more focused on facilitating linkage between datasets, thereby supporting discovery on a variety of novel aspects of SARS-COV-2 behaviour and interactions with host organisms and environments.</p>
+      <p> INSDC members (including the ENA), inadvertently leans towards an object-oriented perspective on Open Science, where the sheer volume of outputs being shared may overshadow diversity and inclusivity</p>
+  </div>
+
+  <div style="flex: 1;">
+      <img src="F5.large.jpg" style="height:100%; width:100%;" alt="">
+      
+      <p>Sheehan et al: Unrestricted versus Regulated Open Data Governance: A Bibliometric Comparison of SARS-CoV-2 Nucleotide Sequence Databases (2023) </p>
+  </div>
+</div>
+
+<div style="border: limegreen dashed 4px;">
+  <h2 style="text-align: center;">Barriers for Infrastructuring with the ENA</h2>
+  <h4 style="margin-left:5%;">1. Issues of sovereignty & accreditation</h4>
+ <p> <i style="text-align: center;">Excerpt from Interview with EO_1_1a_D</i></p>
+  <p style="text-align: center;"> R: So I think one of the issues is people not seeing the value in spending time submitting data.... 
+    One thing that we heard time and time again, was how, if somebody in Latin America were to generate data, you know, that's their data. That's their country's kind of like, sovereignty. In that way. It's kind of this bigger question around like, you know,  is the data personal to the country or personal to them or their institution?....
+    So generally speaking, people in the higher income countries, Western Europe, US, that kind of thing there's this idea that basically this is a kind of research inequity thing and kinda  post colonial, like grabbing and the maintenance of a country's own data in a separate place, a bit akin to the British Museum, and how it could have, yeah, stole loads of things from around the world and kept him there. So I think that's a very valid thing that people from different countries think. 
+</p>
+
+<h4 style="margin-left:5%;">2. Issues of Metadata Control</h4>
+<p> <i style="text-align: center;">Excerpt from Interview with PL_1_1a_D</i></p>
+<h3 style="text-align: center;">R: data without metadata is fundamentally useless, you can't use that information because you have no context from which it came from, and the context in which to place it.</h3>
+</div>
+
+<div>
+  <pre style="font-size:1em;">
+    --------------------------------------------------------------------------------
+    |                             END OF PRESENTATION                                |
+    --------------------------------------------------------------------------------
+
+                    A       T
+                     \     /
+                      C-G
+                     /     \
+                    G       C
+                     \     /
+                      T-A
+                     /     \
+                    A       T
+                     \     /
+                      C-G
+                     /     \
+
+</pre>
+<h2>
+  I am  open to any questions or feedback!
+  Please feel free to reach out via email:
+
+  Email:ns651@exeter.ac.uk</h2>
+
+</div>
+
+
+  </div>
+</div>
+    </div>
+</body>
+
+<!-- The rest of your HTML remains the same -->
+
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+
+  body {
+    background-color: #000;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  .terminal {
+    background-color: black;
+    border: solid 2px limegreen;
+    border-radius: 5px;
+    width: 800px;
+    height: 600px;
+    overflow: auto;
+    margin-left: 5%;
+  }
+
+  p{
+    margin-left: 5%;
+  }
+
+  .header {
+    background-color: limegreen;
+    color: black;
+    padding: 5px;
+    text-align: center;
+    font-family: 'VT323', monospace;
+    font-size: 1.2em;
+    border-bottom: solid 2px limegreen;
+  }
+
+  .container {
+    color: limegreen;
+    font-family: 'VT323', monospace;
+    text-align: left;
+    padding: 20px;
+    overflow-y: auto;
+    height: 90%;
+  }
+
+  h1, h2 {
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-top: 5px;
+  }
+
+  .images {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .images img {
+    margin: 10px;
+    max-width: 40%;
+    height: auto;
+  }
+
+  .workshop-info {
+    border: 2px dashed limegreen;
+    padding: 20px;
+    margin-top: 20px;
+  }
+
+  .workshop-info hr {
+    border: 1px dashed limegreen;
+  }
+</style>
